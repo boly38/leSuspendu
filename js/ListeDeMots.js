74efc8f133bd53543904930884f170efa6fff69b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-25 08:01:39 +0000

    Deploy boly38/leSuspendu to github.com/boly38/leSuspendu.git:gh-pages

---
+++ js/ListeDeMots.js
@@ -0,0 +1,11 @@
+class ListeDeMots {
+  constructor() {
+      this.mots = [
+      "lion","maison","ordinateur","fortnite","casque","lampadaire","vélo","maman","papa",
+      "table","arbre","chaise","chat","chien","eau","panneau","école","cahier","trousse","crayon","gomme",
+      "cheminée","neige","arbre","montagne","boudha","imprimante","feuille","jeu","dessin","télévision","foot",
+      "dictée","assez","affûté","effaré","couvercle","intérieur","aide","effort","effet","boîte","contenu","colis",
+      "pantalon","trempé","escalier","pince","linge", "baby-foot", "suspendu"
+      ];
+  }
+}
